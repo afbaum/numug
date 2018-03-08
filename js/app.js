f20240959824b71bbb63e4787ca94c68346cde2e
Amanda Baum <amanda.baum01@gmail.com>
2018-03-07 21:03:05 -0500

    updating layout for conf. webpage

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
-$( document ).ready(function() {
-  $(".namelist").each(function(i, e){
-    $(this).delay(i*500).animate({fontSize: '150%'},"slow");
-  });
-});
 
-$(document).ready(function(){
-  //Init Scroll ScrollMagic
-  var controller = new ScrollMagic.Controller();
-
-  // build a scene
-  var ourScene = new ScrollMagic.Scene({
-      tiggerElement: '#missiontitle'
-  })
-  .setClassToggle('#missiontitle', 'fade-in') // add class to missiontitle
-  .addTo(controller)
-});
